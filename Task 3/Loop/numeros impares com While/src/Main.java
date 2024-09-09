5005bf97ee834a9ff6f158baf4254334cf3c88a6
maykonzx7 <159338838+maykonzx7@users.noreply.github.com>
2024-09-09 15:31:19 -0300

    Task 3

---
+++ Task 3/Loop/numeros impares com While/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int n = 1;
+        while (n <= 50) {
+            if (n % 2 != 0) {
+                System.out.println(n);
+            }
+            n++;
+        }
+    }
+}
